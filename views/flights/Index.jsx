c29a73809aaf2c989d50f6ca71ca59b5c7b82495
Linda <lexicon736@gmail.com>
2022-09-19 16:39:57 -0400

    minor changes

--- views/flights/Index.jsx
+++ views/flights/Index.jsx
@@ -3,9 +3,23 @@ const React = require('react');
 const Index = (props) => {
   return (
     <div>
+      <h1> Index Route </h1>
+      <a href="/flights/new">Create a New Flight!</a>
       {/* In here goes your list of all of the flights and their info */}
+    
+      {
+          props.flights.map((flight, index) => {
+            return (
+              <li key={index}>
+                The <a href={`/flights/${flight._id}`}>{flight.airline}</a> is {flight.flightNo} {flight.departs ? " datetime-local" : "one year from date created!"}
+              </li>
+            )
+          })
+        }
     </div>
-  )
-}
 
+
+  )
+
+ }
 module.exports = Index;
--- views/flights/New.jsx
+++ views/flights/New.jsx
@@ -1,9 +1,20 @@
+
 const React = require('react');
 
 const New = (props) => {
-  return (
+  return(
     <div>
-      {/* In here goes your form for creating a new Flight */}
+      <h1> New Route </h1>
+
+      <form action="/flights" method="POST">
+        <label>Airline:</label>
+        <input type="text" name="name" /><br/>
+        <label>flightNo:</label>
+        <input type="text" number="number" /><br/>
+        <label>Departs:</label>
+        <input type="datetime-local"></input>
+        <input type="submit" value="Create Flight!" />
+      </form>
     </div>
   )
 }
